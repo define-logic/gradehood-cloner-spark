d84fc80ec3c66c866d9ae2bcdb3560978200fde5
define-logic <100401490+define-logic@users.noreply.github.com>
2026-04-03 17:35:02 +0530

    thanyou.tsx added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import ThankYou from "./pages/ThankYou";
+import ThankYou from "./pages/ThankYou.tsx";
 
 const queryClient = new QueryClient();
 
